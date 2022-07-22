f16b4c10209302231048a607b8a75db1ca0bdb8d
sunsetFog <2693281934@qq.com>
2022-07-22 16:13:24 +0800

    父子工程

---
+++ springcloud-provider/src/main/java/com/core/ProviderCenter.java
@@ -0,0 +1,12 @@
+package com.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 启动类
+@SpringBootApplication
+public class ProviderCenter {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderCenter.class, args);
+    }
+}
